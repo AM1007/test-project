d62e208985e689663f4c70ebcf0bb33b9a7fe759
MOTKO <andrew.motko@gmail.com>
2023-05-05 15:47:54 +0300

    function added

--- scrpt.js
+++ scrpt.js
@@ -1,2 +1,17 @@
-document.body.innerHTML = "<h1>Hello from JS</h1>";
-console.log("test2!!!!");
+// Creating function
+
+// New
+
+const person = new Object();
+
+person.firstName = "First Name";
+person.secondName = "Second Name";
+
+console.log(person);
+
+function Test(first, second) {
+  this.prop1 = first;
+  this.prop2 = second;
+}
+
+console.log(Test());
